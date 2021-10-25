a577cae949a2845f00f2bd4aa022bbb2993fe180
Sriram Balakrishnan <sbalakrishnan@securitycompass.com>
2021-10-24 21:13:53 -0400

    Created HouseListing Model

--- listings/api/models.py
+++ listings/api/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
 # TODO: Create your models here.
+
+
+class HouseListing(models.Model):
+    AREA_UNIT_CHOICES = [("SqFt", "Square Feet")]
+    HOME_TYPE_CHOICES = [
+        ("Apartment", "Apartment"),
+        ("Condominium", "Condominium"),
+        ("Duplex", "Duplex"),
+        ("Miscellaneous", "Miscellaneous"),
+        ("MultiFamily2To4", "Multi-Family 2 To 4"),
+        ("SingleFamily", "Single Family"),
+        ("VacantResidentialLand", "Vacant Residential Land"),
+    ]
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    area_unit = models.CharField(
+        choices=AREA_UNIT_CHOICES,
+        max_length=4,
+        default="SqFt"
+    )
+    home_type = models.CharField(
+        choices=HOME_TYPE_CHOICES,
+        max_length=25,
+        default="Miscellaneous"
+    )
+    home_size = models.PositiveIntegerField(null=True, blank=True)
+    property_size = models.PositiveIntegerField(null=True, blank=True)
+    bathrooms = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    bedrooms = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    year_built = models.IntegerField(null=True, blank=True)
+
+    address = models.CharField(max_length=75)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=8)
+
+    last_sold_date = models.DateField(null=True, blank=True)
+    last_sold_price = models.PositiveIntegerField(null=True, blank=True)
+    link = models.URLField()
+    price = models.PositiveIntegerField(null=True, blank=True)
+    rent_price = models.PositiveIntegerField(null=True, blank=True)
+    rentzestimate_amount = models.PositiveIntegerField(null=True, blank=True)
+    rentzestimate_last_updated = models.DateField(null=True, blank=True)
+    tax_value = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        blank=True
+    )
+    tax_year = models.PositiveIntegerField(null=True, blank=True)
+    zestimate_amount = models.PositiveIntegerField(null=True, blank=True)
+    zestimate_last_updated = models.DateField(null=True, blank=True)
+    zillow_id = models.UUIDField(unique=True, db_index=True)
