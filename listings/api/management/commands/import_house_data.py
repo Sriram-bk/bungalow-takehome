f102271b4fe53e3abd720ea6c845c3a711ce7fae
Sriram Balakrishnan <sbalakrishnan@securitycompass.com>
2021-10-25 01:13:46 -0400

    implmented import_house_data mgmt command

--- listings/api/management/commands/import_house_data.py
+++ listings/api/management/commands/import_house_data.py
@@ -1,12 +1,96 @@
+import csv
+import re
+
+from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
+
+from api.models import HouseListing
+
 
 class Command(BaseCommand):
     help = 'Imports data about houses'
 
     def add_arguments(self, parser):
-        # TODO: Add any arguments here
-        pass
+        parser.add_argument(
+            "--csv",
+            dest="csv",
+            default="",
+            help=("CSV file to import house data from")
+        )
 
     def handle(self, *args, **options):
-        # TODO: implement your import command
-        self.stdout.write("Import to be implemented.")
+        csv_file_name = options.get("csv", None)
+
+        if not csv_file_name:
+            raise CommandError(
+                "No CSV file specified! use --csv <csv_file_name> to specify a CSV file."
+            )
+
+        self.parse_csv_file(csv_file_name)
+
+    def parse_csv_file(self, csv_file_name):
+        with open(csv_file_name, "r") as csv_file:
+            csv_reader = csv.DictReader(csv_file, skipinitialspace=True)
+            house_listings = []
+
+            for row in csv_reader:
+                for key, val in row.items():
+                    if not val:
+                        row[key] = None
+
+                house_listings.append(HouseListing(
+                    area_unit=row["area_unit"],
+                    bathrooms=row["bathrooms"],
+                    bedrooms=row["bedrooms"],
+                    home_size=row["home_size"],
+                    home_type=row["home_type"],
+                    last_sold_date=self.normalize_date(
+                        row["last_sold_date"]
+                    ),
+                    last_sold_price=row["last_sold_price"],
+                    link=row["link"],
+                    price=self.normalize_price(row["price"]),
+                    property_size=row["property_size"],
+                    rent_price=row["rent_price"],
+                    rentzestimate_amount=row["rentzestimate_amount"],
+                    rentzestimate_last_updated=self.normalize_date(
+                        row["rentzestimate_last_updated"]
+                    ),
+                    tax_value=row["tax_value"],
+                    tax_year=row["tax_year"],
+                    year_built=row["year_built"],
+                    zestimate_amount=row["zestimate_amount"],
+                    zestimate_last_updated=self.normalize_date(
+                        row["zestimate_last_updated"]
+                    ),
+                    zillow_id=int(row["zillow_id"]),
+                    address=row["address"],
+                    city=row["city"],
+                    state=row["state"],
+                    zipcode=row["zipcode"]
+                ))
+
+        HouseListing.objects.bulk_create(house_listings)
+
+    def normalize_price(self, price):
+        if not price:
+            return None
+
+        numeral = float(re.findall(r'\d+\.?\d*', price)[0])
+        multiplier_value = price[-1]
+
+        if multiplier_value == 'M':
+            return numeral * 1000000
+
+        if multiplier_value == 'K':
+            return numeral * 1000
+
+        return None
+
+    def normalize_date(self, date):
+        if not date:
+            return None
+
+        return datetime.strptime(
+            date, '%M/%d/%Y'
+        )
